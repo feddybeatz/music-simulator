70abd12eddf8546d8f9c761a078b0cdee9387083
feddybeatz <fednardwambua@gmail.com>
2026-02-18 13:17:27 +0300

    chore(tailwind): migrate off CDN to PostCSS build; add configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './**/*.{html,ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
